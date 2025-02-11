46bfd752b562c73d6b5a1edebe721b3ff04c8083
Vincent Hengel <prelink835@gmail.com>
2025-02-11 15:01:22 +0100

    Render md and emoji better

--- monkeychat/lib/widgets/chat_message.dart
+++ monkeychat/lib/widgets/chat_message.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:latext/latext.dart';
 
 class ChatMessage extends StatelessWidget {
   final String text;
@@ -29,17 +30,91 @@ class ChatMessage extends StatelessWidget {
                 text,
                 style: const TextStyle(color: Colors.white),
               )
-            : MarkdownBody(
+            : Column(
+                crossAxisAlignment: isUser
+                    ? CrossAxisAlignment.end
+                    : CrossAxisAlignment.start,
+                children: _buildContents(text, context),
+              ),
+      ),
+    );
+  }
+
+  List<Widget> _buildContents(String text, BuildContext context) {
+    final List<Widget> widgets = [];
+    final parts = text.split('\n');
+
+    for (final part in parts) {
+      if (part.trim().startsWith('```math') && part.trim().endsWith('```')) {
+        // Extract math content
+        final mathContent = part.replaceAll('```math', '').replaceAll('```', '').trim();
+        widgets.add(
+          LaTexT(
+            laTeXCode: Text(
+              '\$$mathContent\$',
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      } else if (part.contains(r'`$') && part.contains(r'$`')) {
+        // Handle inline math
+        final textParts = part.split(RegExp(r'`\$.*?\$`'));
+        final mathParts = RegExp(r'`\$(.*?)\$`').allMatches(part);
+        
+        final List<Widget> rowWidgets = [];
+        int mathIndex = 0;
+        
+        for (int i = 0; i < textParts.length; i++) {
+          if (textParts[i].isNotEmpty) {
+            rowWidgets.add(
+              MarkdownBody(
                 selectable: true,
-                data: text,
-                styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                data: textParts[i],
+                styleSheet: MarkdownStyleSheet(
                   p: const TextStyle(color: Colors.white),
                   a: const TextStyle(color: Colors.blue),
                   em: const TextStyle(fontStyle: FontStyle.italic),
                   strong: const TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-      ),
-    );
+            );
+          }
+          if (mathIndex < mathParts.length) {
+            final mathContent = mathParts.elementAt(mathIndex).group(1);
+            rowWidgets.add(
+              LaTexT(
+                laTeXCode: Text(
+                  '\$$mathContent\$',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            );
+            mathIndex++;
+          }
+        }
+        
+        widgets.add(
+          Wrap(
+            children: rowWidgets,
+          ),
+        );
+      } else {
+        // Regular markdown
+        widgets.add(
+          MarkdownBody(
+            selectable: true,
+            data: part,
+            styleSheet: MarkdownStyleSheet(
+              p: const TextStyle(color: Colors.white),
+              a: const TextStyle(color: Colors.blue),
+              em: const TextStyle(fontStyle: FontStyle.italic),
+              strong: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        );
+      }
+    }
+
+    return widgets;
   }
 }
